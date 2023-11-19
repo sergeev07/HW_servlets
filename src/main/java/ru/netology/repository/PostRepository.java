b09e8fa7582ba10ea15a1307296918bf6572bc65
Aleks Sergeev <aleks.sergeev07@gmail.com>
2023-11-19 17:11:20 +0700

    add spring-context, di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
@@ -8,7 +9,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+
 // Stub
+@Repository
 public class PostRepository {
     private final ConcurrentMap<Long, Post> allPosts;
     private final AtomicLong idCounter = new AtomicLong();
